02fe1713da08de07cf808643630de77225d3050a
Leo <l.hernando.tan@gmail.com>
2026-02-17 02:14:10 +0800

    auth service model, seed db, and dto

---
+++ auth-service/src/test/java/com/leohernando/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leohernando.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
